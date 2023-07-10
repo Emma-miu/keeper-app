bc3169f88a34cd86d0c9d8013670aea138b70f62
fradin.emma <fradin.emma@appirits.com>
2023-07-10 15:28:51 +0900

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [newNote, setNewNote] = useState([]);
+
+  function addNote(noteContent) {
+    setNewNote((prevNotes) => {
+      return [...prevNotes, noteContent];
+    });
+  }
+
+  function deleteNote(id) {
+    setNewNote((prevNotes) => {
+      return prevNotes.filter((noteContent, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {newNote.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNote}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [noteContent, setNoteContent] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNoteContent((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+
+  const [bool, setBool] = useState(false);
+
+  function handleZoom() {
+    setBool(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={noteContent.title}
+          style={bool ? {} : { display: "none" }}
+        />
+        <textarea
+          onClick={handleZoom}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows={bool ? 3 : 1}
+          value={noteContent.content}
+        />
+        <Zoom in={bool}>
+          <Fab
+            type="button"
+            onClick={() => {
+              props.onAdd(noteContent);
+              setNoteContent({
+                title: "",
+                content: "",
+              });
+            }}
+          >
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
